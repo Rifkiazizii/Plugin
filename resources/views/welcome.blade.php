9c16199f148f71e8b4eff23cda56316b6d4707dc
Rifkiazizii <rifkijr177@gmail.com>
2019-05-25 10:44:21 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    bukber
                 </div>
 
                 <div class="links">
